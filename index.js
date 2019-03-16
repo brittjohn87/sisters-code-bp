1d3a8d90a787c9b94f31847b6f1272e602fd4bcb
brittjohn87 <brittanytjohnson@icloud.com>
2019-03-16 00:22:44 -0400

    Added Final Code

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // jQuery Functions Go Here 
+$('#rock').click(function(){
+	game('rock');
+})
 
+$('#paper').click(function(){
+	game('paper');
+})
+
+$('#scissors').click(function(){
+	game('scissors');
+})
 
 // Computer Range for Game
 // rock = 1-3
@@ -10,12 +20,44 @@
 // Game function starts here
 function game(choice) {
 	// Defining our computer variable
-	var computer;
+	var computer = Math.floor(Math.random() * 10) + 1;
+
 	// if computer chose rock
+	if(computer <= 3){
+		if(choice === 'rock'){
+			$('#result').html("It's A Tie! The computer also chose rock.")
+		}
+		else if(choice === 'paper'){
+			$('#result').html("You win! The computer chose rock.")
+		}
+		else {
+			$('#result').html("You Lose! The computer chose rock.")
+		}
+	}
 	
 	// if computer chose paper
-	
+	else if(computer >= 4 && computer <= 7){
+		if(choice === 'rock'){
+			$('#result').html("You lose! The computer chose paper.")
+		}
+		else if(choice === 'paper'){
+			$('#result').html("It's a tie! The computer also chose paper.")
+		}
+		else {
+			$('#result').html("You win! The computer chose paper.")
+		}
+	}
 	// if computer chose scissors
-	
+	else {
+		if(choice === 'rock'){
+			$('#result').html("You win! The computer chose scissors.")
+		}
+		else if(choice === 'paper'){
+			$('#result').html("You lose! The computer chose scissors.")
+		}
+		else {
+			$('#result').html("It's a tie!")
+		}
+	}
 }
 
